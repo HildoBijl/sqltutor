ad7eed959975e78a32aceed94a1a27dc017ec744
Hildo Bijl <hildobijl@gmail.com>
2025-03-11 18:23:25 +0100

    Define skill tree

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,5 +1,8 @@
 import { Subpage } from 'components'
 
+import { components } from 'edu'
+console.log(components)
+
 export function Overview() {
 	return <Subpage>
 		<p>The overview page of all the skills will appear here.</p>
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
       src: '/src',
       assets: '/src/assets',
       components: '/src/components',
+      edu: '/src/edu',
       pages: '/src/pages',
       routing: '/src/routing',
       settings: '/src/settings',
