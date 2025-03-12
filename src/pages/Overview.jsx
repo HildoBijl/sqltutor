4bd6bf33bce9a79d2a73e9201c97fe01aad23264
Hildo Bijl <hildobijl@gmail.com>
2025-03-12 15:00:47 +0100

    List concepts and skills in overview

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,10 +1,17 @@
 import { Subpage } from 'components'
 
-import { components } from 'edu'
-console.log(components)
+import { concepts, skills } from 'edu'
 
 export function Overview() {
 	return <Subpage>
-		<p>The overview page of all the skills will appear here.</p>
+		<p>Contents are split up into small modular blocks. We distinguish between concepts (an idea like &quot;What is a database?&quot; that can be visualized) and skills (an action like &quot;Write a single-table query&quot; that can be executed). You find the full list of the contents below.</p>
+		<h4>Concepts</h4>
+		<ul>
+			{Object.values(concepts).map(concept => <li key={concept.id}>{concept.name}</li>)}
+		</ul>
+		<h4>Skills</h4>
+		<ul>
+			{Object.values(skills).map(skill => <li key={skill.id}>{skill.name}</li>)}
+		</ul>
 	</Subpage>
 }
