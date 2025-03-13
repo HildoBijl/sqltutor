bc041fbde7eec487919a26d6c35c2147d57f8793
Hildo Bijl <hildobijl@gmail.com>
2025-03-13 15:02:10 +0100

    Fix faulty HTML <title> tag

--- src/components/layout/Page.jsx
+++ src/components/layout/Page.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import { siteTitle } from 'settings'
@@ -16,6 +17,21 @@ export function Page() {
 // The MetaTitle ensures that the <title> tag in the <head> part of the HTML is set properly, ensuring a proper title in the browser tab.
 function MetaTitle() {
 	const route = useCurrentRoute()
-	const tabTitle = `${(route.title ? `${route.title} | ` : '')}${siteTitle}`
-	return <title>{tabTitle}</title>
+
+	// The route title might be a React component, and the HTML <title> tag requires a string. We use a round-about way of getting the inner text rendered in the given route title component.
+	const hiddenTitleRef = useRef()
+	const [headerTitle, setHeaderTitle] = useState()
+	
+	// When the route changes, update the title.
+	useEffect(() => {
+		setHeaderTitle(hiddenTitleRef.current?.innerText)
+	}, [route, hiddenTitleRef])
+	window.r = hiddenTitleRef
+	
+	// Determine the tab title and show it.
+	const tabTitle = `${(headerTitle ? `${headerTitle} | ` : '')}${siteTitle}`
+	return <>
+		<div ref={hiddenTitleRef} style={{ display: 'none' }}>{route.title}</div>
+		<title>{tabTitle}</title>
+	</>
 }
