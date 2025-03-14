020acb12643b1ebfb454242f8c900bd29ab250bb
Hildo Bijl <hildobijl@gmail.com>
2025-03-14 16:01:40 +0100

    Allow case insensitive URLs

--- src/pages/edu/util.js
+++ src/pages/edu/util.js
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom'
 
 import { components } from 'edu'
 
-// tabs indicates which tabs can be shown.
+// tabs indicates which tabs can be shown and which corresponding Module component should be rendered for it.
 export const tabs = [
 	{
 		url: 'story',
@@ -20,18 +20,19 @@ export const tabs = [
 		title: 'Exercises',
 	},
 	{
-		url: 'dataexplorer',
+		url: 'dataExplorer',
 		component: 'DataExplorer',
 		title: 'Data explorer',
 	},
 ]
 
-// useComponent gets the componentId from the URL and loads the component from it.
+// useComponent gets the componentId from the URL and loads the component from it. It also compensates for issues in the case of the given parameter.
 export function useComponent() {
 	const { componentId } = useParams()
-	if (!components[componentId])
+	const component = components[componentId] || Object.values(components).find(component => component.id.toLowerCase() === componentId.toLowerCase())
+	if (!component)
 		throw new Error(`Invalid componentId: the componentId "${componentId}" is not known.`)
-	return components[componentId]
+	return component
 }
 
 // useUrlTab gets the tab from the URL if it is given. It enforces it to be lower case.
