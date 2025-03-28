5aad27e598a7098145d1f7c1e8017a6b50101ca0
Hildo Bijl <hildobijl@gmail.com>
2025-03-28 15:53:55 +0100

    Get rid of outdated import dependency

--- src/pages/edu/Component.jsx
+++ src/pages/edu/Component.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useTheme } from '@mui/material/styles'
-import { useMediaQuery, Tabs, Tab, Box } from '@mui/material'
+import { Tabs, Tab, Box } from '@mui/material'
 
 import { firstOf, useWindowSize } from 'util'
 import { Subpage, Container } from 'components'
