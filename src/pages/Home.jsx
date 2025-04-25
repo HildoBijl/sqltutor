20650a6fd7ff4a9d66e28f18476e8616f15b7c9d
Hildo Bijl <hildobijl@gmail.com>
2025-04-25 16:14:35 +0200

    Fix minor typo in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,5 +7,5 @@ export function Home() {
 		<h1>SQL Valley</h1>
 		<p>The SQL Valley web app will be built here. It is under development.</p>
 		<p>Check out the <Link to="/test">test page</Link> for the database input field, or the <Link to="/overview">overview page</Link> for the skill overview.</p>
-	</Subpage >
+	</Subpage>
 }
