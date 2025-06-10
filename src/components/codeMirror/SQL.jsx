95e0486338401e78a9c8d7ee89bc7ebe932ed552
Hildo Bijl <hildobijl@gmail.com>
2025-06-10 12:09:34 +0200

    Minor adjustment to SQL component style: fix positioning within lines

--- src/components/codeMirror/SQL.jsx
+++ src/components/codeMirror/SQL.jsx
@@ -10,6 +10,6 @@ export function SQL({ lineNumbers = false, foldGutter = false, highlightActiveLi
 		basicSetup={{ lineNumbers, foldGutter, highlightActiveLine, highlightActiveLineGutter: highlightActiveLine, }}
 		{...{ readOnly: true, editable: false, value, ...props }}
 		extensions={[sql(), ...extensions]}
-		style={{ display: 'inline-block' }}
+		style={{ display: 'inline-block', transform: 'translateY(-1pt)' }}
 	/>
 }
