afa1420a276240a0f5cb0c02e7b4324c3103183c
Hildo Bijl <hildobijl@gmail.com>
2025-06-10 14:50:50 +0200

    Set up CodeMirror tabbing using tab-characters of width 2

--- src/components/codeMirror/SQL.jsx
+++ src/components/codeMirror/SQL.jsx
@@ -1,15 +1,18 @@
 import CodeMirror from '@uiw/react-codemirror'
-import { sql } from '@codemirror/lang-sql'
 
 import './style.css'
+import { ownExtensions } from './util'
 
 // Set up the SQL object to use the CodeMirror object with the right default settings.
 export function SQL({ lineNumbers = false, foldGutter = false, highlightActiveLine = false, extensions = [], children, value, ...props }) {
 	value = value || children
+	value = value.trim()
+	if (typeof value !== 'string')
+		throw new Error(`Invalid SQL query: expected a query with only text, but received something that possibly contains HTML components or similar.`)
 	return <CodeMirror
-		basicSetup={{ lineNumbers, foldGutter, highlightActiveLine, highlightActiveLineGutter: highlightActiveLine, }}
+		basicSetup={{ lineNumbers, foldGutter, highlightActiveLine, highlightActiveLineGutter: highlightActiveLine }}
 		{...{ readOnly: true, editable: false, value, ...props }}
-		extensions={[sql(), ...extensions]}
+		extensions={[...ownExtensions, ...extensions]}
 		style={{ display: 'inline-block', transform: 'translateY(-1pt)' }}
 	/>
 }
--- src/components/codeMirror/SQLInput.jsx
+++ src/components/codeMirror/SQLInput.jsx
@@ -1,7 +1,7 @@
 import CodeMirror from '@uiw/react-codemirror'
-import { sql } from '@codemirror/lang-sql'
 
 import './style.css'
+import { ownExtensions } from './util'
 
 // Set up the SQLInput object to use the CodeMirror object with the right default settings.
 export function SQLInput({
@@ -11,6 +11,6 @@ export function SQLInput({
 	return <CodeMirror
 		basicSetup={{ lineNumbers, foldGutter, highlightActiveLine, highlightActiveLineGutter: highlightActiveLine }}
 		{...props}
-		extensions={[sql(), ...extensions]}
+		extensions={[...ownExtensions, ...extensions]}
 	/>
 }
