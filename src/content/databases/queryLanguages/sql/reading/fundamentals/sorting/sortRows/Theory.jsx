2f5e57f03d5d46dd4f2a17bfb0ef949e6ea1ff27
Hildo Bijl <hildobijl@gmail.com>
2025-06-11 08:33:38 +0200

    Textual changes to the SortRows Theory page

--- src/content/databases/queryLanguages/sql/reading/fundamentals/sorting/sortRows/Theory.jsx
+++ src/content/databases/queryLanguages/sql/reading/fundamentals/sorting/sortRows/Theory.jsx
@@ -9,12 +9,11 @@ export function Theory() {
 
 		<Par>When receiving a table filled with data from a query, we might want to order the rows in a certain way. For instance alphabetically by name, sorted by number of employees, or similar. How can we use SQL to sort rows in a table?</Par>
 
-		<Head>Sorting on a single column</Head>
-		<Par>To instruct SQL to sort rows, we add an <SQL>ORDER BY</SQL> command to the end of the query, followed by the name of the column by which it should be ordered.</Par>
+		<Head>Sort on a single column</Head>
+		<Par>To instruct SQL to sort rows, we add an <SQL>ORDER BY</SQL> command to the end of the query, followed by the name of the column by which it should be ordered. By adding the <SQL>ASC</SQL> (ascending) or <SQL>DESC</SQL> (descending) classifiers, we indicate the sorting direction.</Par>
 		<FigureSingleColumnSorting />
-		<Par>By adding the <SQL>ASC</SQL> (ascending) or <SQL>DESC</SQL> (descending) classifiers, we indicate the sorting direction.</Par>
 
-		<Head>Sorting based on multiple columns</Head>
+		<Head>Sort based on multiple columns</Head>
 		<Par>If the sorting column has many equal values, it is helpful to add a second (or even a third and fourth) sorting column. We can do this by giving a list of column sortings, separated by commas. When rows have the same value within the first column, then the second column is used for comparison, and then the third, and so forth.</Par>
 		<Drawing width={800} height={200}>
 			<Rectangle dimensions={[[0, 0], [800, 200]]} style={{ fill: 'blue', opacity: 0.1 }} />
@@ -22,12 +21,14 @@ export function Theory() {
 				<SQL>{`
 SELECT *
 FROM companies
-ORDER BY country ASC, name DESC
+ORDER BY
+	country ASC,
+	name DESC
 			`}</SQL>
 			</Element>
 		</Drawing>
 
-		<Head>Limiting the number of rows</Head>
+		<Head>Limit the number of rows</Head>
 		<Par>If not the whole table is needed, but only the first few rows, then we can limit the number of rows that are given. To do so, we add a <SQL>LIMIT</SQL> command after the <SQL>ORDER BY</SQL> command and specify how many rows we need.</Par>
 		<Drawing width={800} height={200}>
 			<Rectangle dimensions={[[0, 0], [800, 200]]} style={{ fill: 'blue', opacity: 0.1 }} />
@@ -54,8 +55,8 @@ LIMIT 2 OFFSET 1
 		</Drawing>
 		<Warning>Though most database management systems use the <SQL>LIMIT</SQL> and <SQL>OFFSET</SQL> commands, there are a few DBMs that do not stick to this convention. If the usual commands do not work, even on simple queries, check out the specifications for your own DBM.</Warning>
 
-		<Head>Dealing with NULL values</Head>
-		<Par>If there are NULL values in the sorted column, we can indicate manually if we want them at the start or at the end. For this we use the <SQL>NULLS FIRST</SQL> or <SQL>NULLS LAST</SQL> additions.</Par>
+		<Head>Deal with NULL values</Head>
+		<Par>When sorting, NULL values are always considered "larger" than any other values. So when sorting ascending they'll come last, and when sorting descending they'll come first. If this is not what we want, we could manually specify where we want the NULL values by adding <SQL>NULLS FIRST</SQL> or <SQL>NULLS LAST</SQL>. This is optional and can be done separately per sorting column.</Par>
 		<Drawing width={800} height={200}>
 			<Rectangle dimensions={[[0, 0], [800, 200]]} style={{ fill: 'blue', opacity: 0.1 }} />
 			<Element position={[50, 50]} anchor={[0, 0]}>
@@ -66,7 +67,6 @@ ORDER BY country ASC NULLS FIRST
 			`}</SQL>
 			</Element>
 		</Drawing>
-		<Par>By default, NULL values are "larger" than any other value. So the default for ascending sorting is <SQL>NULLS LAST</SQL>, while the default for descending sorting is <SQL>NULLS FIRST</SQL>.</Par>
 	</>
 }
 
