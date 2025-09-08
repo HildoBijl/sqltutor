5b6f832a564166dfa97d41bbbff1d4f4f949943e
Alexandra <alexandra.boala@gmail.com>
2025-09-08 17:35:42 +0200

    Changed the Link method to useNavigate to implement navigation between the SkillTree page and Skill Theory page trough double click (to prevent annoying UI for students)

--- src/pages/Design.jsx
+++ src/pages/Design.jsx
@@ -1,17 +1,23 @@
-import { Link  } from 'react-router-dom'    
+import { Link , useNavigate } from 'react-router-dom'    
 import { Subpage } from 'components'
 import { Rectangle, Drawing, Element, Line} from 'components'
 import { Vector } from 'util'
 
 function Skill({position, title, to}) {
         const skillPos = new Vector(position)
+        const navigate = useNavigate();
 
         const width = 150; 
         const height = 50;
         const rectStart = skillPos.subtract(new Vector(width/2, height/2))
         const rectEnd = skillPos.add(new Vector(width/2, height/2))
 
-        const skillContent = (
+        const handleDoubleClick = () => {
+            if (to) {
+                navigate(to);               
+            }
+        } 
+        return (    
             <>
               <Rectangle
                   dimensions={{start: rectStart, end: rectEnd}}
@@ -19,18 +25,21 @@ function Skill({position, title, to}) {
                   style={{fill: '#f0f0f0', stroke: '#ccc', strokeWidth: 2}}
               />
               <Element position={skillPos}>
-                  <span style={{
-                      color: '#333',
-                      fontWeight: 'bold',
-                      textAlign: 'center',
-                      display: 'block',
-                      fontSize: '18px'
-                  }}>{title}</span>
+                  <span 
+                      onDoubleClick={handleDoubleClick}
+                      style={{
+                          color: '#333',
+                          fontWeight: 'bold',
+                          textAlign: 'center',
+                          display: 'block',
+                          fontSize: '18px',
+                          cursor : to ? 'pointer' : 'default'
+                      }}>{title}</span>
               </Element>
           </>
         )
         
-        return to ? <Link to={to}>{skillContent}</Link>: skillContent
+        
         
         
     }
