5c953b13503beb3484a8ebf679ed54ce343dbeea
Alexandra <alexandra.boala@gmail.com>
2025-09-08 17:22:58 +0200

    Modified the function Skill to link the visual representation in the skill tree with each page for theory. Now, upon clicking on a rectangle with a skill, the user is redirected to the skill theory page.

--- src/pages/Design.jsx
+++ src/pages/Design.jsx
@@ -1,9 +1,9 @@
 import { Link  } from 'react-router-dom'    
 import { Subpage } from 'components'
-import { Rectangle, Drawing, Element} from 'components'
+import { Rectangle, Drawing, Element, Line} from 'components'
 import { Vector } from 'util'
 
-function Skill({position, title}) {
+function Skill({position, title, to}) {
         const skillPos = new Vector(position)
 
         const width = 150; 
@@ -11,22 +11,28 @@ function Skill({position, title}) {
         const rectStart = skillPos.subtract(new Vector(width/2, height/2))
         const rectEnd = skillPos.add(new Vector(width/2, height/2))
 
-        return <>
-        <Rectangle
-        dimensions={{start: rectStart, end: rectEnd}}
-        cornerRadius={10}
-        style={{fill: '#f0f0f0', stroke: '#ccc', strokeWidth: 2}}
-        />
-        <Element position={skillPos}>
-            <span style={{
-                color: '#333',
-                fontWeight: 'bold',
-                textAlign: 'center',
-                display: 'block',
-                fontSize: '18px'
-            }}>{title}</span>
-            </Element>
-        </>        
+        const skillContent = (
+            <>
+              <Rectangle
+                  dimensions={{start: rectStart, end: rectEnd}}
+                  cornerRadius={10}
+                  style={{fill: '#f0f0f0', stroke: '#ccc', strokeWidth: 2}}
+              />
+              <Element position={skillPos}>
+                  <span style={{
+                      color: '#333',
+                      fontWeight: 'bold',
+                      textAlign: 'center',
+                      display: 'block',
+                      fontSize: '18px'
+                  }}>{title}</span>
+              </Element>
+          </>
+        )
+        
+        return to ? <Link to={to}>{skillContent}</Link>: skillContent
+        
+        
     }
 
 
@@ -34,7 +40,8 @@ export function Design() {
     
     return <Subpage>
         <Drawing width={400} height={400}>
-            <Skill position={{x: 200, y: 100}} title="Database" />
+            <Skill position={{x: 200, y: 100}} title="Database" to="/c/database" />
+            <Skill position={{x: 200, y:230}} title="Database Table" to="/c/databaseTable" />
         </Drawing>
             
     </Subpage>  
