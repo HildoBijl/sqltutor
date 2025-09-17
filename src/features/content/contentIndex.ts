6b99e373a8a77e4ac09fa19e62f36de7eef8c52f
Strahil Peykov <strahil.peykov@gmail.com>
2025-09-17 20:43:00 +0200

    Migrate database content to new features structure

---
+++ src/features/content/contentIndex.ts
@@ -0,0 +1,11 @@
+﻿import type { ContentMeta } from '@/features/content';
+import { contentIndex } from '@/features/content';
+
+export type ContentType = ContentMeta['type'];
+export type ContentEntryMeta = ContentMeta;
+
+export async function loadContentIndex(): Promise<ContentEntryMeta[]> {
+  return contentIndex;
+}
+
+export { contentIndex };
