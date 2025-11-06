f002751041dda6f70282a4f161dda83491f6c43a
Hildo Bijl <hildobijl@gmail.com>
2025-11-06 16:26:15 +0100

    Make Drawing Element load faster by immediately adding positioning style data

--- src/components/figures/Drawing/components/htmlComponents/Element.tsx
+++ src/components/figures/Drawing/components/htmlComponents/Element.tsx
@@ -30,25 +30,27 @@ export function Element(props: ElementProps) {
 	const p = useEqualRefOnEquality(ensureVector(position, 2));
 	const a = useEqualRefOnEquality(ensureVector(anchor, 2));
 
+	// Define positioning.
+	const { bounds, figure, getFigureScale } = useDrawingData();
+	const getTransformOrigin = (a: Vector) => `${(a.x + 1) / 2 * 100}% ${(a.y + 1) / 2 * 100}%`;
+	const getTransform = (p: Vector, rotate: number, scale: number) => `
+		translate(${-(a.x + 1) / 2 * 100}%, ${-(a.y + 1) / 2 * 100}%)
+		scale(${getFigureScale()})
+		translate(${p.x}px, ${p.y}px)
+		scale(${scale})
+		rotate(${rotate! * 180 / Math.PI}deg)
+		${style?.transform ?? ''}`
+
 	// Update element position based on current scale and transform.
 	const [mergedRef, internalRef] = useEnsureRef<HTMLDivElement>(ref);
-	const { bounds, figure, getFigureScale } = useDrawingData();
 	const updateElementPosition = useCallback(() => {
 		// On no data, show nothing.
 		const element = internalRef.current;
 		if (!element || !bounds || !figure?.inner)
 			return;
-
 		// Position the element accordingly.
-		element.style.transformOrigin = `${(a.x + 1) / 2 * 100}% ${(a.y + 1) / 2 * 100}%`;
-		element.style.transform = `
-			translate(${-(a.x + 1) / 2 * 100}%, ${-(a.y + 1) / 2 * 100}%)
-			scale(${getFigureScale()})
-			translate(${p.x}px, ${p.y}px)
-			scale(${scale})
-			rotate(${rotate! * 180 / Math.PI}deg)
-			${style?.transform ?? ''}
-		`;
+		element.style.transformOrigin = getTransformOrigin(a);
+		element.style.transform = getTransform(p, rotate!, scale!);
 	}, [internalRef, bounds, figure, getFigureScale, p, a, rotate, scale]);
 
 	// Call update on layout changes and resize.
@@ -61,7 +63,8 @@ export function Element(props: ElementProps) {
 			left: 0,
 			position: 'absolute',
 			top: 0,
-			transformOrigin: '0% 0%',
+			transformOrigin: `${(a.x + 1) / 2 * 100}% ${(a.y + 1) / 2 * 100}%`,
+			transform: getTransform(p, rotate!, scale!),
 			...(behind ? { zIndex: -1 } : { zIndex: 3 }),
 			...(passive ? { ...notSelectable, pointerEvents: 'none' } : {}),
 			...(style ?? {}),
