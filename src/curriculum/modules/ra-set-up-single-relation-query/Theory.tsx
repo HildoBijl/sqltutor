50be5119696560eada9341789888cf43da507aec
Hildo Bijl <hildobijl@gmail.com>
2026-01-12 18:05:53 +0100

    Tiny textual fix for the ra-set-up-single-relation-query pages

--- src/curriculum/modules/ra-set-up-single-relation-query/Theory.tsx
+++ src/curriculum/modules/ra-set-up-single-relation-query/Theory.tsx
@@ -12,15 +12,15 @@ export function Theory() {
       <Par>The first thing we should note is that all the data we need is in a <Em>single</Em> relation. In that case, we can simply set up a <Term>single-relation query</Term>. The plan/steps for that are as follows.</Par>
       <List useNumbers items={[
         <>
-          <Par>Determine the <Term>relation</Term> that you will need. For us that's the <RelationName>employees</RelationName> relation. We can write it down for our query as</Par>
+          <Par>Determine the <Term>relation</Term> that has the required data. For us that's the <RelationName>employees</RelationName> relation. We can write it down for our query as</Par>
           <RA style={{ marginTop: 8, marginBottom: 8 }}>employees</RA>
         </>,
         <>
-          <Par>Apply <Term>filtering</Term> to get only the tuples that we need. This turns the query into</Par>
+          <Par>Apply <Term>filtering</Term> to only get the required tuples. This turns the query into</Par>
           <RA style={{ marginTop: 8, marginBottom: 8 }}>σ<sub>salary &gt; 200000</sub>(employees)</RA>
         </>,
         <>
-          <Par>Apply <Term>projection</Term> to only extract the desired attributes. This gives the final result</Par>
+          <Par>Apply <Term>projection</Term> to only extract the required attributes. This gives the final result</Par>
           <RA style={{ marginTop: 8, marginBottom: 8 }}>∏<sub>first_name,last_name</sub>(σ<sub>salary &gt; 200000</sub>(employees))</RA>
         </>,
       ]} />
