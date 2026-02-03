a6f3850ec8ad599ee820d6069df05969b9f84d64
Hildo Bijl <hildobijl@gmail.com>
2026-02-03 16:21:47 +0100

    Add use-filtered-aggregation Summary page and note at exercises

--- src/curriculum/modules/use-filtered-aggregation/Theory.tsx
+++ src/curriculum/modules/use-filtered-aggregation/Theory.tsx
@@ -5,14 +5,14 @@ import { FigureSingleTable, FigureExampleQuery } from '@/curriculum/utils/queryF
 export function Theory() {
   return <Page>
     <Section>
-      <Par>We know how to aggregate columns using SQL, and we know how to apply filters. It is of course also possible to do both. However, there's an important distinction between applying a filter <Em>before</Em> and applying a filter <Em>after</Em> aggregation. Let's take a look at how it works.</Par>
+      <Par>We know how to aggregate columns using SQL, and we know how to apply filters. It is of course also possible to do both together. However, there's an important distinction between applying a filter <Em>before</Em> and applying a filter <Em>after</Em> aggregation. Let's take a look at how it works.</Par>
     </Section>
 
-    <Section title={<>Filter rows before aggregating using <ISQL>WHERE</ISQL></>}>
+    <Section title={<>Filter rows <Em>before</Em> aggregating using <ISQL>WHERE</ISQL></>}>
       <Par>Let's consider the financial performance of the company per quarter.</Par>
       <FigureSingleTable query={`SELECT * FROM quarterly_performance`} tableScale={0.8} tableWidth={800} />
       <Par>Suppose that we want to get the average revenue for the <Em>first half</Em> of each year (January through June). How would we do that?</Par>
-      <Par>The key is to add a <ISQL>WHERE</ISQL> clause to select the rows that we want, throwing out the ones that we don't. So we start with </Par>
+      <Par>The key is to first add a <ISQL>WHERE</ISQL> clause to select the rows that we want, throwing out the ones that we don't.</Par>
       <FigureExampleQuery query={`
 SELECT *
 FROM quarterly_performance
@@ -26,9 +26,9 @@ GROUP BY fiscal_year;`} tableScale={0.8} tableWidth={220} />
       <Par>Note that <ISQL>GROUP BY</ISQL> is written <Em>after</Em> the <ISQL>WHERE</ISQL> clause. This is to signify that the grouping (and subsequently the aggregation) is done <Em>after</Em> running the <ISQL>WHERE</ISQL> clause.</Par>
     </Section>
 
-    <Section title={<>Filter rows after aggregating using <ISQL>HAVING</ISQL></>}>
-      <Par>Suppose that we only care about the years whose average revenue in quarters 1 and 2 exceeded five million. In this case, we want to apply a filter to the <Em>aggregated</Em> table. Specifically, we want to filter on the new column "average_revenue_q12".</Par>
-      <Par>The long and inconvenient way to do so, would be through a <Term>subquery</Term>.</Par>
+    <Section title={<>Filter rows <Em>after</Em> aggregating using <ISQL>HAVING</ISQL></>}>
+      <Par>Extending on the previous example, suppose that we only care about the years whose average revenue in quarters 1 and 2 exceeded five million. In this case, we want to apply a filter to the <Em>aggregated</Em> table. Specifically, we want to filter on the new column "average_revenue_q12".</Par>
+      <Par>The overly long and inconvenient way to do so, would be through a <Term>subquery</Term>.</Par>
       <FigureExampleQuery query={`
 SELECT *
 FROM (
@@ -38,7 +38,7 @@ FROM (
   GROUP BY fiscal_year
 ) 
 WHERE average_revenue_q12 > 5000000;`} tableScale={0.8} tableWidth={220} />
-      <Par>Note that we took our aggregated table and treated it as a new table. It works, but it's a bit cumbersome, and since this is a use-case that occurs often in practice, SQL has a cleaner solution. We add a <ISQL>HAVING</ISQL> clause <Em>after</Em> the <ISQL>GROUP BY</ISQL> clause.</Par>
+      <Par>Note that we took our aggregated table and treated it as a new table. It works, but it's a bit cumbersome, and since this is a use-case that occurs often in practice, SQL has a cleaner solution. We add a <ISQL>HAVING</ISQL> clause <Em>after</Em> the <ISQL>GROUP BY</ISQL> clause to filter the resulting table.</Par>
       <FigureExampleQuery query={`
 SELECT fiscal_year, AVG(revenue) AS average_revenue_q12
 FROM quarterly_performance
@@ -47,45 +47,44 @@ GROUP BY fiscal_year
 HAVING AVG(revenue) > 5000000;`} tableScale={0.8} tableWidth={220} />
       <Par>This does the exact same thing, and it looks a lot cleaner!</Par>
 
-      <Par>The <ISQL>HAVING</ISQL> clause works identically to the <ISQL>WHERE</ISQL> clause: you can set up any filter you like. However, keep in mind that the <ISQL>HAVING</ISQL> clause operates on the <Em>aggregated</Em> table. Just as within the <ISQL>SELECT</ISQL> clause, you may only use <Em>grouping columns</Em> and the <Em>results of aggregated functions</Em> within the filter. You cannot refer to the original columns, like "quarter" or similar, as those are not available anymore. (And doing so would also make no sense.)</Par>
+      <Par>The <ISQL>HAVING</ISQL> clause works the same to the <ISQL>WHERE</ISQL> clause: you can set up any filter you like. However, keep in mind that the <ISQL>HAVING</ISQL> clause operates on the <Em>aggregated</Em> table. Just as within <ISQL>SELECT</ISQL>, you may only use <Em>grouping columns</Em> and <Em>results of aggregation functions</Em>. Using the columns from the original table here will make no sense, and usually results in an error.</Par>
       <Warning>
         <Par sx={{ mb: 1 }}>You may be wondering why we wrote <ISQL>{`HAVING AVG(revenue) > 5000000`}</ISQL> rather than <ISQL>{`HAVING average_revenue_q12 > 5000000`}</ISQL>. The latter would be clearer!</Par>
         <Par sx={{ mb: 1 }}>The problem here is the evaluation order that SQL uses. The <ISQL>SELECT</ISQL> clause (and with that the creation of the "average_revenue_q12" alias) is only run <Em>after</Em> the <ISQL>HAVING</ISQL> clause. So at the <ISQL>HAVING</ISQL> clause, the name "average_revenue_q12" does not exist yet! Most DBMSs will throw an error if you try to use an alias in your <ISQL>HAVING</ISQL> clause. Some DBMSs, like SQLite, do allow it through a work-around, but it's still not recommended.</Par>
-        <Par>The solution is to write the same calculation again. It's not ideal, but it works. And you can of course use any other aggregation functions within the <ISQL>HAVING</ISQL> clause too, even ones that you don't use in the <ISQL>SELECT</ISQL> clause.</Par>
+        <Par>The solution is to write the same calculation again. It's a bit double, but it works. If we <Em>really</Em> don't want to repeat our calculations, we can use a subquery after all.</Par>
       </Warning>
     </Section>
 
-    <Section title="Use further aggregation possibilities">
-      <Par>Now that we're getting skilled at using aggregations, it's worthwhile to look at a few more creative possibilities of using aggregation.</Par>
-
-      <Par>It is common to <Term>preprocess values before aggregation</Term>. Suppose that we want to find the average budget per employee for all departments. Then we first calculate the budget per employee for each department, and subsequently find their average.</Par>
+    <Section title="Process numeric values before/after aggregation">
+      <Par>Aggregation often goes hand with the further processing of column values. This can be done both before and after aggregation.</Par>
+      <Par>First let's check out processing <Em>before</Em> aggregation. Suppose that we want to find the average budget per employee for all departments. To do so, we first calculate the budget per employee for each department, and subsequently find their average.</Par>
       <FigureExampleQuery query={`
 SELECT AVG(budget / nr_employees) AS average_budget_per_employee_ratio
 FROM departments;`} tableScale={0.8} tableWidth={220} />
-      <Par>Note that we perform arithmetics, running a calculation for every row, <Em>before</Em> doing the aggregation. We can also do further preprocessing, like using <ISQL>COALESCE</ISQL> to set up a fallback value in case of <ISQL>NULL</ISQL> values. The <ISQL>CASE WHEN ... THEN</ISQL> function provides even more possibilities.</Par>
-
-      <Par>Another common thing to do is using the results of aggregated functions: we <Term>process values after aggregation</Term>. Suppose that we want to get an overview of the cities where employees live, how many people live there, and most of all <Em>which percentage</Em> of the total live in each city.</Par>
+      <Par>Using basic arithmetics like this is just one example of processing before aggregation. We can also use <ISQL>COALESCE</ISQL> to set up a fallback value in case of <ISQL>NULL</ISQL> values, or we can use <ISQL>CASE WHEN ... THEN ...</ISQL> to deal with even more edge cases.</Par>
+      <Par>When we get results from aggregation, we can use these in our calculations too, effectively processing data <Em>after</Em> aggregation. Suppose that we want to get an overview of the cities where employees live. Using aggregation, we can count how many people live in each city. We could also calculate this as a percentage of the total employee count.</Par>
       <FigureExampleQuery query={`
 SELECT
   city,
   COUNT(*) AS num_people,
-  COUNT(*) * 100.0 / (SELECT COUNT(*) FROM employees) AS percentage
+  COUNT(*) * 100 / (SELECT COUNT(*) FROM employees) AS percentage
 FROM employees
 GROUP BY city;`} tableScale={0.8} tableWidth={320} />
-      <Par>Note that we have casually included a subquery within our calculations. SQL also allows that. After all, the subquery returns a single number, which can be used to calculate with.</Par>
-    
-      <Par>Though not as common, it's also possible to <Term>aggregate text</Term>. Most aggregation functions like <ISQL>SUM</ISQL> and <ISQL>AVG</ISQL> don't work for text, but a few are still relevant.</Par>
+      <Par>Note that we use <ISQL>COUNT</ISQL> and then start calculating with it, multiplying it by <ISQL>100</ISQL>. Also note that we have casually included a subquery within our calculations for the total number of employees. SQL allows subqueries wherever their value make sense. In this case, the subquery returns a single number, so SQL will treat the subquery as a number and use it in its calculations.</Par>
+    </Section>
+
+    <Section title="Use aggregation on text">
+      <Par>Though not as common, it is possible to <Term>aggregate text</Term>. Most aggregation functions like <ISQL>SUM</ISQL> and <ISQL>AVG</ISQL> don't work for text, but a few are still relevant.</Par>
       <FigureExampleQuery query={`
 SELECT
   MIN(d_name) AS first_in_alphabet,
   MAX(d_name) AS last_in_alphabet
 FROM departments;`} tableScale={0.8} tableWidth={250} />
-
-      <Par>One particular function that is useful in aggregating text is the <Term>string concatenation</Term> command. It concatenates all the values within a column, adding a separator in-between. It varies per DBMS what it's called: <ISQL>STRING_AGG</ISQL>, <ISQL>GROUP_CONCAT</ISQL>, or <ISQL>LISTAGG</ISQL>. We can for instance make a list of four-letter codes for each department.</Par>
+      <Par>One particular function that is useful in aggregating text is the <Term>string concatenation</Term> command. It concatenates all the values within a column, adding a separator in-between. It varies per DBMS what it's called: <ISQL>STRING_AGG</ISQL>, <ISQL>GROUP_CONCAT</ISQL>, or <ISQL>LISTAGG</ISQL>. We can for instance make a list of four-letter codes for each department and write them as a dash-separated list.</Par>
       <FigureExampleQuery query={`
-SELECT GROUP_CONCAT(SUBSTRING(d_name, 1, 4), " - ") AS department_codes
+SELECT GROUP_CONCAT(SUBSTRING(d_name, 1, 4), " - ") AS department_codes_list
 FROM departments;`} tableScale={0.8} tableWidth={200} />
-      <Par>Admittedly, it's not the most sensible use-case, but the idea is clear: a <Em>lot</Em> is possible. The above examples are only the tip of the iceberg.</Par>
+      <Par>Admittedly, it's not the most sensible use-case, but the idea is clear: a <Em>lot</Em> is possible with aggregation.</Par>
     </Section>
   </Page>;
 }
--- src/curriculum/modules/use-filtered-aggregation/exercise.ts
+++ src/curriculum/modules/use-filtered-aggregation/exercise.ts
@@ -4,11 +4,11 @@ const EXERCISES: StaticExercise[] = [
   {
     id: 'filtered-aggregation-micro',
     version: 1,
-    prompt: 'Retrieve the buyer ID and the number of transactions they appear in, where the amount of the transaction is less than 5, but that buyer\'s total amount spent is more than 20000.',
+    prompt: '[Exercise is under development: data may not be present yet.] Any transaction whose amount is less than 5 is considered a microtransaction. Create an overview of the buyers (their IDs) and the number of microtransactions they have made. Limit the output to buyers whose total amount spent is more than 20000.',
     solution: `
 SELECT 
-    buyer_id,
-    COUNT(*) AS num_micro_tx
+  buyer_id,
+  COUNT(*) AS num_micro_tx
 FROM transactions
 WHERE amount < 5.00        
 GROUP BY buyer_id
@@ -18,12 +18,12 @@ HAVING SUM(amount) > 20000;
   {
     id: 'filtered-aggregation-teambuilding',
     version: 1,
-    prompt: 'Retrieve the department ID, average, and total expenses for teambuilding activities for departments where the average teambuilding expense exceeds 10,000.',
+    prompt: '[Exercise is under development: data may not be present yet.] Any expenses that have "teambuilding" in their description are considered teambuilding expenses. Create an overview of the departments (their IDs), their total teambuilding expenses, and their average teambuilding expenses per expense listing. Limit the output to departments whose average teambuilding expenses exceed 10,000.',
     solution: `
 SELECT 
-    d_id,
-    AVG(amount) AS avg_expense,
-    SUM(amount) AS total_expense
+  d_id,
+  SUM(amount) AS total_expenses,
+  AVG(amount) AS average_expenses
 FROM expenses
 WHERE description LIKE '%teambuilding%'
 GROUP BY d_id
@@ -33,26 +33,26 @@ HAVING AVG(amount) > 10000;
   {
     id: 'filtered-aggregation-perf-range',
     version: 1,
-    prompt: 'Retrieve the employee ID, minimum, and maximum performance scores for employees whose performance score range exceeds 40.',
+    prompt: 'Create an overview of all employees (their IDs) and their lowest and highest performance score ever obtained. Limit the output to fluctuating employees: those where the difference between the lowest and highest score exceeds 40.',
     solution: `
 SELECT 
-    e_id,
-    MIN(perf_score) AS min_score,
-    MAX(perf_score) AS max_score
+  e_id,
+  MIN(perf_score) AS lowest_score,
+  MAX(perf_score) AS highest_score
 FROM emp_data
 GROUP BY e_id
-HAVING max_score - min_score > 40;
+HAVING MAX(perf_score) - MIN(perf_score) > 40;
     `,
   },
   {
     id: 'filtered-aggregation-rejected-tx',
     version: 1,
-    prompt: 'Retrieve the vendor ID, total number of transactions, and number of rejected transactions for vendors with more than 50 rejected transactions.',
+    prompt: '[Exercise is under development: data may not be present yet.] Create an overview of all vendors (their IDs), their total number of transactions, and the number of rejected transactions. Limit the output to those vendors with more than 50 rejected transactions.',
     solution: `
 SELECT 
-    vendor_id,
-    COUNT(*) AS total_tx,
-    SUM(CASE WHEN status = 'rejected' THEN 1 ELSE 0 END) AS rejected_tx
+  vendor_id,
+  COUNT(*) AS total_tx,
+  SUM(CASE WHEN status = 'rejected' THEN 1 ELSE 0 END) AS rejected_tx
 FROM transactions
 GROUP BY vendor_id
 HAVING SUM(CASE WHEN status = 'rejected' THEN 1 ELSE 0 END) > 50;
@@ -61,14 +61,14 @@ HAVING SUM(CASE WHEN status = 'rejected' THEN 1 ELSE 0 END) > 50;
   {
     id: 'filtered-aggregation-product-revenue',
     version: 1,
-    prompt: 'Retrieve the product ID, total revenue, and number of transactions for products with an average transaction amount below 10 but total revenue above 20,000.',
+    prompt: '[Exercise is under development: data may not be present yet.] Create an overview of all products (their IDs), the number of times it has been sold, and the total revenue obtained from the product. Limit the output to products whose total revenue is more than 20,000, yet the average transaction value is less than 10.',
     solution: `
 SELECT 
-    p_id,
-    SUM(amount) AS revenue,
-    COUNT(*) AS tx_count
+    prod_id,
+    COUNT(*) AS tx_count,
+    SUM(amount) AS revenue
 FROM transactions
-GROUP BY p_id
+GROUP BY prod_id
 HAVING AVG(amount) < 10 AND SUM(amount) > 20000;
     `,
   },
