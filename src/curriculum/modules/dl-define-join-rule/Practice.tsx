bbec0f28169065d8a716f6c927830f058b88b080
Hildo Bijl <hildobijl@gmail.com>
2026-02-27 17:26:57 +0100

    Add dl-define-join-rule Theory and Summary page

---
+++ src/curriculum/modules/dl-define-join-rule/Practice.tsx
@@ -0,0 +1,9 @@
+import { Page, Section, Par } from '@/components';
+
+export function Practice() {
+	return <Page>
+		<Section>
+			<Par>This exercise set is still under development.</Par>
+		</Section>
+	</Page>;
+}
