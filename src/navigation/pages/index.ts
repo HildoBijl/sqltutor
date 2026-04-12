f377746e44a40bd9dfdd3018b8bbbbaa79e1e3f7
Hildo Bijl <hildobijl@gmail.com>
2026-04-12 10:10:26 +0200

    Fix faulty import paths in navigation/pages

--- src/navigation/pages/index.ts
+++ src/navigation/pages/index.ts
@@ -1,8 +1,5 @@
 export { default as HomePage } from './HomePage';
-export { default as SqlLearningOverviewPage } from 'src/navigation/pages/LearningOverview/SqlLearningOverviewPage.tsx';
-export { default as RALearningOverviewPage } from 'src/navigation/pages/LearningOverview/RALearningOverviewPage.tsx';
-export { default as DatalogLearningOverviewPage } from 'src/navigation/pages/LearningOverview/DatalogLearningOverviewPage.tsx';
 export { default as ConceptPage } from './ConceptPage';
 export { default as SkillPage } from './SkillPage';
 export { default as PlaygroundPage } from './PlaygroundPage';
-
+export * from './LearningOverview';
